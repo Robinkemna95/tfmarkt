897d323c771d00da391d38593534b6fd711bca68
DTayeb78 <djambo@web.de>
2017-05-05 08:46:01 +0200

    Testklasse hinzugefügt

---
+++ tfmarkt/Produktklassen/Testklasse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tfmarkt.Produktklassen
+{
+    class Testklasse
+    {
+    }
+}
